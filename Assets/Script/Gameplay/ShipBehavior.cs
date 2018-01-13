a085fd8929e6fafc62a48cd5316a506de84bfa74
Bruno Assarisse <asteriscko@gmail.com>
2018-01-13 18:45:58 -0200

    Added ship with movement and offscreen "teleport"

---
+++ Assets/Script/Gameplay/ShipBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehavior : MonoBehaviour {
+
+	public Rigidbody2D TargetBody;
+	public float MoveForceMultiplier = 0.2f;
+	public float TurnRate = 3f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		if (InputExtensions.Holding.Up) {
+			this.TargetBody.AddForce (this.TargetBody.transform.up * this.MoveForceMultiplier);
+		}
+
+		if (InputExtensions.Holding.Right) {
+			this.TargetBody.transform.Rotate (0, 0, -this.TurnRate);
+		}
+
+		if (InputExtensions.Holding.Left) {
+			this.TargetBody.transform.Rotate (0, 0, this.TurnRate);
+		}
+		
+	}
+}
---
+++ Assets/Script/Gameplay/SpaceTeleport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceTeleport : MonoBehaviour {
+
+	public float AppearOffset = 1f;
+
+	public void Teleport() {
+
+		var camera = Camera.main;
+		var area = camera.rect;
+		var pos = this.transform.position;
+
+		var posMin = camera.ViewportToWorldPoint (new Vector3 (area.xMin, area.yMin, 0));
+		var posMax = camera.ViewportToWorldPoint (new Vector3 (area.xMax, area.yMax, 0));
+		var xMin = posMin.x;
+		var xMax = posMax.x;
+		var yMin = posMin.y;
+		var yMax = posMax.y;
+
+		if (pos.x < xMin) {
+			this.transform.position = new Vector3 (xMax + AppearOffset, pos.y, pos.z);
+		} else if (pos.x > xMax) {
+			this.transform.position = new Vector3 (xMin - AppearOffset, pos.y, pos.z);
+		}
+
+		pos = this.transform.position;
+
+		if (pos.y < yMin) {
+			this.transform.position = new Vector3 (pos.x, yMax + AppearOffset, pos.z);
+		} else if (pos.y > yMax) {
+			this.transform.position = new Vector3 (pos.x, yMin - AppearOffset, pos.z);
+		}
+
+	}
+}
