4d81c651157779cfe6d608ac647d20d78790facd
Bruno Assarisse <asteriscko@gmail.com>
2018-01-19 02:54:29 -0200

    Added scenes for controls and highscores

---
+++ Assets/Script/Screens/Navigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void GoToTitle() {
+		SceneManager.LoadScene ("Title");
+	}
+
+	public void CheckHighScores() {
+		SceneManager.LoadScene ("HighScores");
+	}
+
+	public void CheckControls() {
+		SceneManager.LoadScene ("Controls");
+	}
+
+}
