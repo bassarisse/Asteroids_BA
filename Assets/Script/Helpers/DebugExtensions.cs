a0c59e1a10b3b4e1d197c401eae81c43dde335c1
Bruno Assarisse <asteriscko@gmail.com>
2018-04-02 21:12:57 -0300

    Code formatting

--- Assets/Script/Helpers/DebugExtensions.cs
+++ Assets/Script/Helpers/DebugExtensions.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class DebugExtensions {
 
-	public static void DrawRect (Rect rect, Color color, float z, float duration) {
+    public static void DrawRect (Rect rect, Color color, float z, float duration) {
         var p1 = new Vector3(rect.xMin, rect.yMin, z);
         var p2 = new Vector3(rect.xMax, rect.yMin, z);
         var p3 = new Vector3(rect.xMax, rect.yMax, z);
@@ -12,6 +12,6 @@ public class DebugExtensions {
         Debug.DrawLine(p2, p3, color, duration);
         Debug.DrawLine(p3, p4, color, duration);
         Debug.DrawLine(p4, p1, color, duration);
-	}
+    }
 
 }
